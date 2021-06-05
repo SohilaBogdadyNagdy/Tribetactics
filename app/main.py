a133bccfc578591e6c1696fbba230f5ac0107131
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-06-05 14:46:20 +0200

    Restaurant Model

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
 from . import db
+from .models import User, Restaurant
 
 main = Blueprint('main', __name__)
 
@@ -14,3 +15,28 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/restaurants')
+@login_required
+def restaurants():
+    restaurants = Restaurant.query.all()
+    return render_template('restaurants.html', name=current_user.name, restaurants=restaurants)
+
+
+@main.route('/restaurants', methods=['POST'])
+@login_required
+def restaurants_post():
+    name = request.form.get('name')
+
+    exist = Restaurant.query.filter_by(name=name).first()
+
+    if exist: 
+        flash('Restaurant Name already exists')
+        return redirect(url_for('main.restaurants'))
+
+    new_restaurant = Restaurant(name=name, owner=current_user)
+
+    db.session.add(new_restaurant)
+    db.session.commit()
+
+    return redirect(url_for('main.profile'))
--- app/models.py
+++ app/models.py
@@ -1,8 +1,33 @@
 from flask_login import UserMixin
+from sqlalchemy_utils import ChoiceType
 from . import db
 
 class User(UserMixin, db.Model):
+    TYPES = [
+        (u'ADMIN', u'admin'),
+        (u'USER', u'user'),
+        (u'RESTAURANT', u'restaurant')
+    ]
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    type = db.Column(ChoiceType(TYPES))
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    owner=db.Column(db.Integer, db.ForeignKey('user.id'))
+    #menu = db.Column(db.ARRAY(db.JSON))
+
+class Order(db.Model):
+    STATES = [
+        (u'PENDING', u'Pending'),
+        (u'DELIVERED', u'Delivered'),
+    ]
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(ChoiceType(STATES))
+    user=db.Column(db.Integer, db.ForeignKey('user.id'))
+    restaurant = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    #items = db.Column(db.ARRAY(db.JSON),server_default="{}")
+    totalAmount = db.Column(db.Integer)
