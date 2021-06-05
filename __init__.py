e68d5d3351f70b8ee4a275459af5c824ae5b30e5
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-06-05 19:54:11 +0200

    Procfile

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .app import db, create_app
-
+import os
+os.environ.setdefault("FLASK_APP", "APP")
 db.create_all(app=create_app()) # pass the create_app result so Flask-SQLAlchemy gets the configuration.
